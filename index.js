3fcd41af1e7bb653599f465ce57c7c19d4271a3b
Giselle Smith <gisellec60@gmail.com>
2023-03-02 20:21:01 -0500

    complete loops

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const newArray= [];
+function writeCards(name,event) { 
+    for (let i = 0; i < name.length; i ++ ) { 
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+        debugger
+    }
+        return newArray
+}   
+function countDown(num) {
+    while (num !== 0) {
+        console.log(num--)
+    }
+    console.log(num)
+}          
